ea88bf34f89c2d0f3117efd3b35e3c58ef54d8b2
Mark <mark.lu0913@gmail.com>
2026-02-18 01:07:23 -0500

    small party bug

--- features/Party/partySounds.js
+++ features/Party/partySounds.js
@@ -4,13 +4,13 @@ import { chat, playSound } from "../../util/utils";
 
 registerPacketChat((message) => {
     if (!c.partyInviteSound && !c.partyJoinSound) return;
-    if (/^-+\n.*? has invited you to join their party!\nYou have 60 seconds to accept\. Click here to join!\n-+$/.test(message)
-        || /^-+\n.*? has invited you to join .*?'s party!\nYou have 60 seconds to accept\. Click here to join!\n-+$/.test(message)) {
+    if (c.partyInviteSound && (/^-+\n.*? has invited you to join their party!\nYou have 60 seconds to accept\. Click here to join!\n-+$/.test(message)
+        || /^-+\n.*? has invited you to join .*?'s party!\nYou have 60 seconds to accept\. Click here to join!\n-+$/.test(message))) {
 
         playSound(c.partyInviteSoundType ?? "entity.cat.ambient", c.partyInviteSoundVolume, c.partyInviteSoundPitch / 100);
     }
 
-    else if (message.match(/^Party Finder > \w{3,16} joined the dungeon group! \(\w+ Level \d{1,2}\)$/) || message.match(/^Party Finder > \w{3,16} joined the group! \(\w+ Level \d{1,2}\)$/)) {
+    else if (c.partyJoinSound && (message.match(/^Party Finder > \w{3,16} joined the dungeon group! \(\w+ Level \d{1,2}\)$/) || message.match(/^Party Finder > \w{3,16} joined the group! \(\w+ Level \d{1,2}\)$/))) {
         playSound(c.partyJoinSoundType, c.partyJoinSoundVolume, c.partyJoinSoundPitch / 100)
     }
     
